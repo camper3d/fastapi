d6ac751a82b0be37b3a445b92321f50ede7d9eb3
camper3d <camper3d@gmail.com>
2025-04-07 11:21:56 +0300

    exceptions

--- app1/users/dependencies.py
+++ app1/users/dependencies.py
@@ -3,10 +3,11 @@ from jose import jwt, JWTError
 from app1.config import settings
 from datetime import datetime
 from app1.users.DAO import UsersDAO
+from app1.exceptions import TokenExpiredException, TokenAbsentException, IncorrectTokenFormatException, UserIsNotPresentException
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token") # Получение куки
     if not token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -17,15 +18,17 @@ async def get_current_user(token: str = Depends(get_token)):
             token, settings.SECRET_KEY, settings.ALGORITHM
         )
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectTokenFormatException
     expire: str = payload.get('exp')
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredException
     user_id: str = payload.get('sub')
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
 
     return user
+
+
--- app1/users/router.py
+++ app1/users/router.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, HTTPException, status, Response
+from fastapi import APIRouter, Response, Depends
 from app1.users.auth import get_password_hash
 from app1.users.DAO import UsersDAO
 
 from app1.users.schemas import SchemaUserAuth
 from app1.users.auth import authenticate_user, create_access_token
+from app1.users.dependencies import get_current_user
+from app1.users.models import Users
+from app1.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
 
 router = APIRouter(
     prefix='/auth',
@@ -14,7 +17,7 @@ router = APIRouter(
 async def register_user(user_data: SchemaUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email) # Проверка пользователя
     if existing_user:
-        raise HTTPException(status_code=500) # Если пользователь есть, возвращаем ошибку
+        raise UserAlreadyExistsException # Если пользователь есть, возвращаем ошибку
     hashed_password = get_password_hash(user_data.password) # Хеширование пароля пользователя
     await UsersDAO.add(email=user_data.email, hashed_password=hashed_password) # Заносим в базу данных емаил и хешированный пароль
 
@@ -23,7 +26,22 @@ async def register_user(user_data: SchemaUserAuth):
 async def login_user(response: Response, user_data: SchemaUserAuth):
     user = await authenticate_user(user_data.email, user_data.password) # Проверка маила, юзера, пароля
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED) # Ошибка авторизации
+        raise IncorrectEmailOrPasswordException # Ошибка авторизации
     access_token = create_access_token({'sub': str(user.id)}) # Создание токен доступа(обязательно строчный тип)
     response.set_cookie('booking_access_token', access_token, httponly=True) # Сэтим куку, httponly=True, чтобы нельзя было достать куку через js
     return {'access_token': access_token}
+
+
+@router.post("/logout")
+async def logout_user(response:Response):
+    response.delete_cookie('booking_access_token')
+
+
+@router.get("/me")
+async def read_user_me(current_user:Users = Depends(get_current_user)):
+    return current_user
+
+
+
+
+
