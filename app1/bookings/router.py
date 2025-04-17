1d73e3a67f7ea9daa37724cf91e677e323eb5194
camper3d <camper3d@gmail.com>
2025-04-17 17:16:03 +0300

    bookings, query

--- app1/bookings/router.py
+++ app1/bookings/router.py
@@ -1,8 +1,10 @@
+from datetime import date
 from fastapi import APIRouter, Request, Depends
 from app1.bookings.DAO import BookingDAO
 from app1.bookings.schemas import SchemaBooking
 from app1.users.dependencies import get_current_user
 from app1.users.models import Users
+from app1.exceptions import RoomCannotBeBooked
 
 
 
@@ -14,7 +16,17 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings(user: Users = Depends(get_current_user)):
+async def get_bookings(user: Users = Depends(get_current_user)) -> list[SchemaBooking]:
     return await BookingDAO.find_all(user_id=user.id)
+
+
+@router.post("")
+async def add_booking(
+        room_id: int, date_from: date, date_to: date,
+        user: Users = Depends(get_current_user),
+):
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBooked
 
 
--- app1/main.py
+++ app1/main.py
@@ -14,35 +14,27 @@ app = FastAPI()
 app.include_router(router_users)
 app.include_router(router_bookings)
 
-class HotelsSearchArgs:
-    def __init__(
-        self,
-        location: str,
-        date_from: date,
-        date_to: date,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-        has_spa: Optional[bool] = None
-    ):
-        self.location = location
-        self.date_from = date_from
-        self.date_to = date_to
-        self.stars = stars
-        self.has_spa = has_spa
+class SchemaHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
 
 
 @app.get('/hotels')
 def get_hotels(
-        search_args: HotelsSearchArgs = Depends()
-):
-    return search_args
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+) -> list[SchemaHotel]:
+    hotels = [
+        {
+            "address": "ул.Гагарина, 1, Алтай",
+            "name": "Super Hotel",
+            "stars": 5,
+        },
+    ]
+    return hotels
 
 
-class SchemaBooking(BaseModel):
-    room_id: int
-    date_from: date
-    date_ti: date
-
-
-@app.post('/bookings')
-def add_booking(booking: SchemaBooking):
-    pass
